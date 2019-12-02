0fa056bac8864b7b5d93c0f8ef9f8df5d6a6b0fa
Shigas <India1233@users.noreply.github.com>
2019-12-02 16:53:31 +0530

    Initial Commit

---
+++ CoreDatacodable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDatacodable
+//
+//  Created by Suresh Shiga on 02/12/19.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
